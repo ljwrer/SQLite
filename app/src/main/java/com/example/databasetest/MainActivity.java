e1703c8d5f82fd8bd3be23598dc02c73bf3e9a6e
ljwrer <ljwrer@sina.com>
2015-05-19 17:38:30 +0800

    A1

---
+++ app/src/main/java/com/example/databasetest/MainActivity.java
@@ -0,0 +1,148 @@
+package com.example.databasetest;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+    private MyDatabaseOpenHelper helper;
+    Button add;
+    Button update;
+    Button delete;
+    Button query;
+    TextView result;
+    Button replace;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        helper=new MyDatabaseOpenHelper(MainActivity.this,"bookStore.db",null,2);
+        Button button=(Button)findViewById(R.id.create_database);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                helper.getWritableDatabase();
+            }
+        });
+        add=(Button)findViewById(R.id.add_data);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues values=new ContentValues();
+                SQLiteDatabase db=helper.getWritableDatabase();
+                values.put("name","The Da Vinci Code");
+                values.put("author","Dan Brown");
+                values.put("pages",454);
+                values.put("price",16.96);
+                db.insert("Book", null, values);
+                values.clear();
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values);
+                values.clear();
+                Toast.makeText(MainActivity.this,"insert success",Toast.LENGTH_SHORT).show();
+            }
+        });
+        update=(Button)findViewById(R.id.update_data);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues values=new ContentValues();
+                SQLiteDatabase db=helper.getWritableDatabase();
+                values.put("price",10.99);
+                db.update("Book", values, "name=?", new String[]{"The Da Vinci Code"});
+                Toast.makeText(MainActivity.this,"update success",Toast.LENGTH_SHORT).show();
+            }
+        });
+        delete=(Button)findViewById(R.id.delete_data);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db=helper.getWritableDatabase();
+                db.delete("Book","pages>?",new String[]{"500"});
+                Toast.makeText(MainActivity.this,"delete success",Toast.LENGTH_SHORT).show();
+            }
+        });
+        query=(Button)findViewById(R.id.query_data);
+        result=(TextView)findViewById(R.id.query_result);
+        query.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db=helper.getWritableDatabase();
+                Cursor cursor=db.query("Book",null,null,null,null,null,null);
+                if(cursor.moveToFirst())
+                {
+                    do
+                    {
+                        String author=cursor.getString(cursor.getColumnIndex("author"));
+                        String name=cursor.getString(cursor.getColumnIndex("name"));
+                        int pages=cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price=cursor.getDouble(cursor.getColumnIndex("price"));
+                        result.setText(author+" "+name+" "+pages+" "+price);
+                    }
+                    while (cursor.moveToNext());
+
+                }
+            }
+        });
+        replace =(Button)findViewById(R.id.replace_data);
+        replace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db=helper.getWritableDatabase();
+                db.beginTransaction();
+                try{
+                    db.delete("Book",null,null);
+                    if(true)
+                    {
+                        throw new NullPointerException();
+                    }
+                    ContentValues values=new ContentValues();
+                    values.put("name","The Da Vinci Code");
+                    values.put("author","Dan Brown");
+                    values.put("pages",454);
+                    values.put("price",16.96);
+                    db.insert("Book", null, values);
+                    values.clear();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    db.endTransaction();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/databasetest/MyDatabaseOpenHelper.java
@@ -0,0 +1,45 @@
+package com.example.databasetest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Created by Ray on 2015/5/19.
+ */
+public class MyDatabaseOpenHelper extends SQLiteOpenHelper {
+    public static final String books="create table Book("
+            +"id integer primary key autoincrement, "
+            +"author text, "+"price real, "
+            +"pages integer, "
+            +"name text, "
+            +"category_id integer)";
+    public static final String category="create table Category("
+            +"id integer primary key autoincrement, "
+            +"category_name text, "
+            +"category_code integer)";
+    private Context mContext;
+    public MyDatabaseOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext=context;
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(books);
+        db.execSQL(category);
+        Toast.makeText(mContext,"create database success",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        switch (oldVersion)
+        {
+            case 1:
+                db.execSQL(category);
+            case 2:
+                db.execSQL("alter table Book add column category_id integer");
+            default:
+        }
+    }
+}
